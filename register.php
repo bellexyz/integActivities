5ee2f14790bf236abd627a383ba3f5f9999b008c
bellexyz <145647121+bellexyz@users.noreply.github.com>
2023-10-20 12:10:34 +0800

    activity3

--- register.php
+++ register.php
@@ -1,21 +1,17 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Database connection parameters
-    $hostname = "localhost";  // Replace with your MySQL server hostname
-    $username = "root";       // Replace with your MySQL username
-    $password = "";           // Replace with your MySQL password
-    $database = "registration"; // Replace with your MySQL database name
-
+    $hostname = "localhost";  
+    $username = "root";       
+    $password = "";           
+    $database = "registration"; 
     try {
         $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        // Retrieve data from the form
         $name = $_POST['name'];
         $email = $_POST['email'];
         $password = $_POST['password'];
 
-        // Insert data into the database
         $stmt = $conn->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
         $stmt->bindParam(':name', $name);
         $stmt->bindParam(':email', $email);
