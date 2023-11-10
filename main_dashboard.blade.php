43c8cd7d789de885a729d40d485d69312d61d706
bellexyz <145647121+bellexyz@users.noreply.github.com>
2023-11-10 22:29:17 +0800

    Add files via upload
    activity_5

--- main_dashboard.blade.php
+++ main_dashboard.blade.php
@@ -4,9 +4,7 @@
 <html>
 <head>
     <title>Main Dashboard</title>
-    <!-- Add your CSS or other head elements if needed -->
 </head>
 <body>
-    <!-- Your main dashboard content here -->
 </body>
 </html>
